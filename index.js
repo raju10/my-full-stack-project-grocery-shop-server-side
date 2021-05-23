be7c0859d904b1852490d25be5d422cdd69594e8
raju10 <trolltube272@gmail.com>
2021-05-23 16:43:14 +0600

    update

--- index.js
+++ index.js
@@ -37,13 +37,11 @@ client.connect((err) => {
   });
 
   app.get("/ourProduct", (req, res) => {
-    const search = req.query.search;
+    // const search = req.query.search;
     //  { catagory: { $regex: search } }
-    productCollection
-      .find({ catagory: { $regex: search } })
-      .toArray((err, items) => {
-        res.send(items);
-      });
+    productCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
   });
   //////////////
   app.post("/addAdmain", (req, res) => {
